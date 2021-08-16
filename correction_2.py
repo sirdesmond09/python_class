ff16bc20ba51b0ce9c4f606a714db139e80d902e
sirdesmond09 <sirdesmond09@gmail.com>
2021-08-16 12:08:31 +0100

    correction for assignment

---
+++ correction_2.py
@@ -0,0 +1,15 @@
+#1 get one string from two given strings
+
+string1 = "ABC"
+string2 = "XYZ"
+
+print(string1+ string2)
+
+#2 find the area of trapezium, h=5, a=5, b=6
+
+h = 5
+a = 5
+b = 6
+
+area = ((a+b)*h)/2
+print(area)
